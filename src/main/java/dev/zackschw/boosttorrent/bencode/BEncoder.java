e5f31aaf4c72f30ff17f6277d0df76c1f7c6c437
claycot <claycotton@hotmail.com>
2020-02-05 23:27:31 -0500

    [Clay] Implemented B-Encode.

--- src/main/java/dev/zackschw/boosttorrent/bencode/BEncoder.java
+++ src/main/java/dev/zackschw/boosttorrent/bencode/BEncoder.java
@@ -1,9 +1,62 @@
 package dev.zackschw.boosttorrent.bencode;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.List;
+
 
 public class BEncoder {
-    public static void write(Object o, OutputStream output) {
+    @SuppressWarnings("unchecked")
+    public static void write(Object o, OutputStream output) throws IOException {
+        if (o instanceof Number) {
+            //return "i<int>e"
+            writeNum((Number) o, output);
+        }
+        else if (o instanceof String) {
+            //return "<len>:<text>"
+            writeString(((String) o).getBytes(StandardCharsets.UTF_8), output);
+        }
+        else if (o instanceof byte[]) {
+            //return "<len>:<text>"
+            writeString((byte[]) o, output);
+        }
+        else if (o instanceof List) {
+            //return "l<content>e"
+            output.write('l');
+            for (Object listElem : (List<Object>) o) {
+                write(listElem, output);
+            }
+            output.write('e');
+        }
+        else if (o instanceof Map) {
+            //if object is a dictionary
+            //return "d<content>e"
+            //iterate over elements of the map and call write for them
+            output.write('d');
+            for (Map.Entry<String, Object> mapElem : ((Map<String, Object>)o).entrySet()) {
+                writeString(mapElem.getKey().getBytes(StandardCharsets.UTF_8), output);
+                write(mapElem.getValue(), output);
+            }
+            output.write('e');
+        }
+        else {
+            //handle error somehow
+            throw new RuntimeException("BEncode has received bad input.");
+        }
+    }
 
+    public static void writeNum(Number i, OutputStream output) throws IOException {
+        output.write('i');
+        output.write(Long.toString(i.longValue()).getBytes(StandardCharsets.UTF_8));
+        output.write('e');
+    }
+
+    public static void writeString(byte[] b, OutputStream output) throws IOException {
+        //write len:string
+        output.write((Integer.toString(b.length)).getBytes(StandardCharsets.UTF_8));
+        output.write(':');
+        output.write(b);
     }
 }
