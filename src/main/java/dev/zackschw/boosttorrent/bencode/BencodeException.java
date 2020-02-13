276bdeb9a1beca44e8ed186cd0029ba4e0f8c768
Zack Schwartz <zackschw@gmail.com>
2020-02-12 21:15:17 -0500

    Metadata skeleton

--- src/main/java/dev/zackschw/boosttorrent/bencode/BencodeException.java
+++ src/main/java/dev/zackschw/boosttorrent/bencode/BencodeException.java
@@ -4,7 +4,7 @@ package dev.zackschw.boosttorrent.bencode;
  * Exception occurred during BDecoding
  */
 public class BencodeException extends Exception {
-    BencodeException(String message, Object... args) {
+    public BencodeException(String message, Object... args) {
         super(String.format(message, args));
     }
 }
