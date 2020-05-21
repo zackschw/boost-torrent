a34aeb23f8c532ee43fb7abb1ac22093e05b48ac
Zack Schwartz <zackschw@gmail.com>
2020-05-21 19:49:34 -0400

    Handshake skeleton

--- src/main/java/dev/zackschw/boosttorrent/Peer.java
+++ src/main/java/dev/zackschw/boosttorrent/Peer.java
@@ -1,12 +1,46 @@
 package dev.zackschw.boosttorrent;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Peer {
     private final PeerAddress peerAddress;
+    private final MetadataInfo meta;
+    private final byte[] myPeerID;
 
-    public Peer(PeerAddress peerAddress) {
+    private DataInputStream din;
+    private DataOutputStream dout;
+
+    private byte[] peerID;
+
+    /**
+     * Creates an unconnected peer.
+     */
+    public Peer(PeerAddress peerAddress, MetadataInfo meta, byte[] myPeerID) {
         this.peerAddress = peerAddress;
+        this.meta = meta;
+        this.myPeerID = myPeerID;
+    }
 
-        // will probably need more arguments
-            // no problem dude I got you
+    void runConnection() {
+        try {
+            Socket sock = new Socket(peerAddress.getAddress(), peerAddress.getPort());
+            din = new DataInputStream(sock.getInputStream());
+            dout = new DataOutputStream(sock.getOutputStream());
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Handshakes the peer and sets peerID on success.
+     */
+    private boolean handshake() {
+        // TODO
+        return false;
     }
 }
