8ebf3a5778c17a8b9766207288a4262c76088982
Zack Schwartz <zackschw@gmail.com>
2020-05-24 12:57:05 -0400

    Sending messages skeleton

--- src/main/java/dev/zackschw/boosttorrent/PeerConnectionOut.java
+++ src/main/java/dev/zackschw/boosttorrent/PeerConnectionOut.java
@@ -10,4 +10,40 @@ public class PeerConnectionOut {
         this.peer = peer;
         this.dout = dout;
     }
+
+    void sendChoke() {
+
+    }
+
+    void sendUnchoke() {
+
+    }
+
+    void sendInterested() {
+
+    }
+
+    void sendUninterested() {
+
+    }
+
+    void sendHave(int piece) {
+
+    }
+
+    void sendBitfield(byte[] bitmap) {
+
+    }
+    
+    void sendRequest(int piece, int begin, int length) {
+
+    }
+
+    void sendPiece(int piece, int begin, byte[] block) {
+
+    }
+
+    void sendCancel(int piece, int begin, int length) {
+
+    }
 }
