e7372d10fb76eb5b8d9bc80acd8448af6d8c3faf
claycot <claycotton@hotmail.com>
2020-05-25 17:13:23 -0400

    [Clay] Implemented basic messages.

--- src/main/java/dev/zackschw/boosttorrent/Message.java
+++ src/main/java/dev/zackschw/boosttorrent/Message.java
@@ -1,7 +1,7 @@
 package dev.zackschw.boosttorrent;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 /**
  * Object representing a message. Messages are sent to an output stream depending on their type.
@@ -10,105 +10,135 @@ public class Message {
     final static byte CHOKE = 0;
     final static byte UNCHOKE = 1;
     final static byte INTERESTED = 2;
-    final static byte UNINTERESTED = 3;
+    final static byte NOTINTERESTED = 3;
     final static byte HAVE = 4;
     final static byte BITFIELD = 5;
     final static byte REQUEST = 6;
     final static byte PIECE = 7;
     final static byte CANCEL = 8;
 
+    /**
+     * Sending keep-alive message not implemented due to non-seeding bittorrent implementation
+     */
 
     /**
      * Sends a Choke message.
-     * @param out output stream to send to
+     * @param dout data output stream to send to
      * @throws IOException if an I/O error occurs writing to the output stream
      */
-    public void sendChoke(OutputStream out) throws IOException {
-        // TODO
+    public static void sendChoke(DataOutputStream dout) throws IOException {
+        //<len=1><id=1>
+        dout.writeInt(1);
+        dout.write(CHOKE & 0xFF);
     }
 
     /**
      * Sends an Unchoke message.
-     * @param out output stream to send to
+     * @param dout data output stream to send to
      * @throws IOException if an I/O error occurs writing to the output stream
      */
-    public void sendUnchoke(OutputStream out) throws IOException {
-        // TODO
+    public static void sendUnchoke(DataOutputStream dout) throws IOException {
+        //<len=1><id=1>
+        dout.writeInt(1);
+        dout.write(UNCHOKE & 0xFF);
     }
 
     /**
      * Sends an Interested message.
-     * @param out output stream to send to
+     * @param dout data output stream to send to
      * @throws IOException if an I/O error occurs writing to the output stream
      */
-    public void sendInterested(OutputStream out) throws IOException {
-        // TODO
+    public static void sendInterested(DataOutputStream dout) throws IOException {
+        //<len=1><id=2>
+        dout.writeInt(1);
+        dout.write(INTERESTED & 0xFF);
     }
 
     /**
      * Sends a Not Interested message.
-     * @param out output stream to send to
+     * @param dout data output stream to send to
      * @throws IOException if an I/O error occurs writing to the output stream
      */
-    public void sendNotInterested(OutputStream out) throws IOException {
-        // TODO
+    public static void sendNotInterested(DataOutputStream dout) throws IOException {
+        //<len=1><id=3>
+        dout.writeInt(1);
+        dout.write(NOTINTERESTED & 0xFF);
     }
 
     /**
      * Sends a Have message.
-     * @param out output stream to send to
+     * @param dout data output stream to send to
      * @param pieceIndex zero-based index of a piece that has just been successfully downloaded and verified via hash
      * @throws IOException if an I/O error occurs writing to the output stream
      */
-    public void sendHave(OutputStream out, int pieceIndex) throws IOException {
-        // TODO
+    public static void sendHave(DataOutputStream dout, int pieceIndex) throws IOException {
+        //<len=5><id=4><piece index>
+        dout.writeInt(5);
+        dout.write(HAVE & 0xFF);
+        dout.writeInt(pieceIndex);
     }
 
     /**
      * Sends a Bitfield message.
-     * @param out output stream to send to
+     * @param dout data output stream to send to
      * @param bitfield bitfield representing the pieces that have been successfully downloaded
      * @throws IOException if an I/O error occurs writing to the output stream
      */
-    public void sendBitfield(OutputStream out, byte[] bitfield) throws IOException {
-        // TODO
+    public static void sendBitfield(DataOutputStream dout, byte[] bitfield) throws IOException {
+        //<len=1+bitfield.length><id=5><bitfield>
+        dout.writeInt(1 + bitfield.length);
+        dout.write(BITFIELD & 0xFF);
+        dout.write(bitfield);
     }
 
     /**
      * Sends a Request message.
-     * @param out output stream to send to
+     * @param dout data output stream to send to
      * @param index the zero-based piece index
      * @param begin the zero-based byte offset within the piece
      * @param length the requested length
      * @throws IOException if an I/O error occurs writing to the output stream
      */
-    public void sendRequest(OutputStream out, int index, int begin, int length) throws IOException {
-        // TODO
+    public static void sendRequest(DataOutputStream dout, int index, int begin, int length) throws IOException {
+        //<len=13><id=6><index><begin><length>
+        dout.writeInt(13);
+        dout.write(REQUEST & 0xFF);
+        dout.writeInt(index);
+        dout.writeInt(begin);
+        dout.writeInt(length);
     }
 
     /**
      * Sends a Piece message.
-     * @param out output stream to send to
+     * @param dout data output stream to send to
      * @param index the zero-based piece index
      * @param begin the zero-based byte offset within the piece
      * @param block the block of data, which is a subset of the piece specified by index
      * @throws IOException if an I/O error occurs writing to the output stream
      */
-    public void sendPiece(OutputStream out, int index, int begin, byte[] block) throws IOException {
-        // TODO
+    public static void sendPiece(DataOutputStream dout, int index, int begin, byte[] block) throws IOException {
+        //<len=9+block.length><id=7><index><begin><block>
+        dout.writeInt(9 + block.length);
+        dout.write(PIECE & 0xFF);
+        dout.writeInt(index);
+        dout.writeInt(begin);
+        dout.write(block);
     }
 
     /**
      * Sends a Cancel message.
-     * @param out output stream to send to
+     * @param dout data output stream to send to
      * @param index the zero-based piece index
      * @param begin the zero-based byte offset within the piece
      * @param length the requested length
      * @throws IOException if an I/O error occurs writing to the output stream
      */
-    public void sendCancel(OutputStream out, int index, int begin, int length) {
-        // TODO
+    public static void sendCancel(DataOutputStream dout, int index, int begin, int length) throws IOException {
+        //<len=13><id=8><index><begin><length>
+        dout.writeInt(13);
+        dout.write(CANCEL & 0xFF);
+        dout.writeInt(index);
+        dout.writeInt(begin);
+        dout.writeInt(length);
     }
-
-
 }
--- src/main/java/dev/zackschw/boosttorrent/PeerConnectionOut.java
+++ src/main/java/dev/zackschw/boosttorrent/PeerConnectionOut.java
@@ -23,7 +23,7 @@ public class PeerConnectionOut {
 
     }
 
-    void sendUninterested() {
+    void sendNotInterested() {
 
     }
 
